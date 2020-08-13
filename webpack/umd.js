046f6a9845d82bd9a173a573597132c5610dea48
Cristiano Betta <cristiano@betta.io>
2020-08-13 18:35:53 +0100

    Remove UMD build from package.

--- webpack/umd.js
+++ webpack/umd.js
@@ -2,11 +2,14 @@ const path = require(`path`)
 const { merge } = require(`webpack-merge`)
 const common = require(`./common.js`)
 
+const { name, version } = require(`../package.json`)
+const filename = `${name.replace(`@`, ``).replace(`/`,`-`)}-${version}.js`
+
 // Builds a version of the library suitable for deployment to our CDN.
 module.exports = merge(common, {
   mode: `production`,
   output: {
-    filename: `gr4vy-embed.js`,
+    filename,
     path: path.resolve(`./umd`),
     libraryTarget: `umd`,
     library: `gr4vy`
