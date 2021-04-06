9f79ff09dc739365a20f7ca5d44aea5ce6cda528
douglaseggleton <douglas.eggleton@outlook.com>
2021-04-06 14:38:09 +0100

    Add theme spec. to Embed

--- packages/embed/src/types.ts
+++ packages/embed/src/types.ts
@@ -17,10 +17,110 @@ export type Config = {
   country: string
   iframeHost: string
   apiHost: string
+  theme?: ThemeOptions // Theming options
 }
 
 export type SetupConfig = Omit<Config, 'iframeHost' | 'apiHost'> & {
   gr4vyId?: string
   iframeHost?: string
   apiHost?: string
+}
+
+type BorderWidth = 'none' | 'thin' | 'thick' // maps to 0, 1, 2
+
+type Radii = 'none' | 'subtle' | 'rounded' // maps to 0, 2, 4
+
+type FontSize = 'small' | 'medium' | 'large' // maps to 0.875rem, 1rem, 1.125rem
+
+export type ThemeOptions = {
+  fonts?: {
+    /**
+     * System or a valid Google font that will be downloaded (should support weights 400,600 and 700)
+     */
+    body?: string
+  }
+  borderWidths?: {
+    /**
+     * Bounding boxes that are not inputs.
+     */
+    container?: BorderWidth
+    /**
+     * Inputs, Radios and Checkbox borders
+     */
+    input?: Omit<BorderWidth, 'none'>
+    /**
+     * Focus ring around interactive elements
+     */
+    focus?: Omit<BorderWidth, 'none'>
+  }
+  colors?: {
+    /**
+     * Used to highlight interactive elements/call-to-actions.
+     */
+    primary?: string
+    /**
+     * Text is set globally for all text, subtle is for hints.
+     */
+    text?: string
+    subtleText?: string
+    /**
+     * Bounding boxes that are not input elements
+     */
+    containerBackgroundUnchecked?: string
+    containerBackground?: string
+    containerBorder?: string
+    /**
+     * Main page background
+     */
+    pageBackground?: string
+    inputBorder?: string
+    inputText?: string
+    inputBackgroud?: string
+    /**
+     * Info is used for loading bars or displaying information.
+     */
+    info?: string
+    infoBackground?: string
+    /**
+     * Danger is used for error messages and destructive actions
+     */
+    danger?: string
+    dangerBackground?: string
+    /**
+     * Focus ring around interactive elements
+     */
+    focus?: string
+  }
+  radii?: {
+    /**
+     * Input Radii
+     */
+    input?: Radii
+    /**
+     * Bounding boxes that are not inputs.
+     */
+    container?: Radii
+  }
+  fontSize?: {
+    /**
+     * Base size of all fonts (relative to the user/browser default)
+     * Screens less than the small screen breakpoint will automatically
+     * reduce their font size.
+     */
+    body?: FontSize
+  }
+  ui?: {
+    /**
+     * Prevents the loading bar from being displayed
+     */
+    loadingStatusBar?: boolean
+    /**
+     * Spaces options apart from each other
+     */
+    gapBetweenOptions?: boolean
+    /**
+     * Dark mode will add white to a base hue instead of black.
+     */
+    darkMode?: boolean
+  }
 }
