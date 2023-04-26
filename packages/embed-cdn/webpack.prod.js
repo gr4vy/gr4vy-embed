d750e1ac4487470806c08cda298d1088db218d16
Andrei Haidukevich <andrei@gr4vy.com>
2023-04-26 13:26:58 +0200

    fix: cdn version

--- packages/embed-cdn/webpack.prod.js
+++ packages/embed-cdn/webpack.prod.js
@@ -1,8 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 const { DefinePlugin } = require('webpack')
+const { execSync } = require("child_process");
 
-const PACKAGE_VERSION = JSON.stringify(process.env.PACKAGE_VERSION || undefined)
+const getCommitHash = () => {
+  try {
+    return `#${execSync('cat .git/`cat .git/HEAD | cut -d \\  -f 2`')
+      .toString()
+      .trim()}`
+  } catch (err) {
+    return ''
+  }
+}
+
+const PACKAGE_VERSION = JSON.stringify(process.env.PACKAGE_VERSION || undefined) || getCommitHash()
 console.log('Building version', PACKAGE_VERSION)
 
 module.exports = {
