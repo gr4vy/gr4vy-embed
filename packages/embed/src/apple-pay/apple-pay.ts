a752a857796e1e1e8d259ee77804da65997d721d
Gary Evans <gary@gr4vy.com>
2024-05-22 14:40:52 +0100

    chore: log onpaymentauthorized event

--- packages/embed/src/apple-pay/apple-pay.ts
+++ packages/embed/src/apple-pay/apple-pay.ts
@@ -34,6 +34,7 @@ export const createApplePayController = (
 
       // handle payment authorization
       session.onpaymentauthorized = (event) => {
+        console.log('onpaymentauthorized', event.payment)
         subjectManager.applePayAuthorized$.next(event.payment.token)
       }
 
