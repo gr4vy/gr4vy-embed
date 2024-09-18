6667b259477fc657057e2896e24762819ba37e43
Luca Allievi <luca@gr4vy.com>
2024-09-18 16:07:14 +0200

    Remove nx reset

--- packages/embed/dev/index.ts
+++ packages/embed/dev/index.ts
@@ -53,8 +53,8 @@ setup({
   form: `#form`,
   intent: 'capture',
   amount: 1299,
-  currency: `AUD`,
-  buyerExternalIdentifier: 'user-009',
+  currency: `GBP`,
+  buyerExternalIdentifier: 'user-001',
   ...instance,
   token: process.env.TOKEN ?? `123456`,
   debug: true,
@@ -76,8 +76,8 @@ setup({
   },
 
   billingAddressFields: {},
-  environment: 'production',
-  country: 'AU',
+  environment: 'sandbox',
+  country: 'GB',
   display: 'all',
   metadata: {
     foo: 'bar',
@@ -91,6 +91,6 @@ setup({
       <p>Payment Method ID: ${transaction.paymentMethod?.id}</p>
       `
   },
-  secure: false,
+  secure: true,
   showDeleteButton: true,
 })
