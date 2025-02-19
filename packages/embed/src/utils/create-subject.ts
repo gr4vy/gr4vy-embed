1ee5d5b40df7587d1773eed17e3e2d9601a10d73
Luca Allievi <luca@gr4vy.com>
2025-02-19 16:05:34 +0100

    Throttle subject next function

--- packages/embed/src/utils/create-subject.ts
+++ packages/embed/src/utils/create-subject.ts
@@ -1,3 +1,5 @@
+import throttle from 'lodash.throttle'
+
 export const createSubject = <T = void>(initialValue?: T) => {
   let value = initialValue
   const subscribers = []
@@ -10,13 +12,22 @@ export const createSubject = <T = void>(initialValue?: T) => {
         },
       }
     },
-    next: (nextValue: T) => {
-      value = nextValue
-      // setTimeout will ensure events are called async
-      subscribers.forEach((callbackFn) =>
-        setTimeout(() => callbackFn(value), 0)
-      )
-    },
+    // throttle the next function call to ensure it's not called
+    // more than once in a short period of time.
+    next: throttle(
+      (nextValue: T) => {
+        value = nextValue
+        // setTimeout will ensure events are called async
+        subscribers.forEach((callbackFn) =>
+          setTimeout(() => callbackFn(value), 0)
+        )
+      },
+      100,
+      {
+        leading: false, // don't execute first call.
+        trailing: true, // execute the last call.
+      }
+    ),
     value: () => {
       return value
     },
