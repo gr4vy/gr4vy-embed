351f6517baa97a6bb36ed1c3d427a808727d91c6
Luca Allievi <101414321+luca-gr4vy@users.noreply.github.com>
2025-04-29 18:27:23 +0200

    chore: update react dev app to mimic the standard SDK one (#311)

--- packages/embed-react/dev/index.tsx
+++ packages/embed-react/dev/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { memo, useState } from 'react'
 import ReactDOM from 'react-dom'
 import Embed from '../src'
 
-ReactDOM.render(
-  <Embed
-    intent="capture"
-    amount={1299}
-    currency="USD"
-    iframeHost={process.env.IFRAME_HOST ?? '127.0.0.1:8080'}
-    apiHost={process.env.API_HOST ?? '127.0.0.1:3100'}
-    token="123456"
-    debug
-    country="US"
-    onEvent={console.log}
-    locale="en"
-  />,
-  document.querySelector('#app')
-)
+const MemoEmbed = memo(function MemoEmbed({
+  onComplete,
+}: {
+  onComplete: CallableFunction
+}) {
+  return (
+    <form action="/" id="payment-form">
+      <Embed
+        form="#payment-form"
+        intent="capture"
+        amount={1299}
+        currency="AUD"
+        buyerExternalIdentifier="user-009"
+        iframeHost={process.env.IFRAME_HOST ?? '127.0.0.1:8080'}
+        apiHost={process.env.API_HOST ?? '127.0.0.1:3100'}
+        token={process.env.TOKEN ?? '123456'}
+        debug
+        country="AU"
+        onEvent={(name, data) =>
+          console.debug(`Merchant page received ${name}`, data)
+        }
+        locale="en"
+        onComplete={(transaction) => onComplete(transaction)}
+      />
+      <input type="submit" />
+    </form>
+  )
+})
+
+const App = () => {
+  const [transaction, setTransaction] = useState(null)
+
+  return (
+    <div style={{ maxWidth: '500px' }}>
+      <MemoEmbed onComplete={setTransaction} />
+      {transaction && (
+        <>
+          <p>Transaction ID: {transaction.id}</p>
+          <p>Status: {transaction.status}</p>
+          <p>Payment Method ID: {transaction.paymentMethod?.id}</p>
+        </>
+      )}
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
