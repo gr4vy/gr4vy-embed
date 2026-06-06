c25c2e403901ed3a1796e4a2d954e2d45cb51fa1
GiordanoArman <giordano@gr4vy.com>
2026-06-06 15:58:07 +0200

    draft solution for Sentry issue https://gr4vy-tq.sentry.io/issues/7486375527/?alert_rule_id=15723108&alert_type=issue&notification_uuid=d68908db-b3c0-434c-b0db-640ed0ec7cdb&project=4508636754477056&referrer=slack

--- packages/embed/src/popup/redirect-popup.ts
+++ packages/embed/src/popup/redirect-popup.ts
@@ -4,6 +4,16 @@ export type RedirectPopup = {
   redirect: (url: string) => void
 }
 
+const isCrossOriginError = (error: unknown): boolean => {
+  if (!(error instanceof Error)) return false
+  // SecurityError (Chrome, Firefox) or DOMException with code 18 (Safari)
+  return (
+    error.name === 'SecurityError' ||
+    (error instanceof DOMException && error.code === 18) ||
+    error.message.includes('cross-origin')
+  )
+}
+
 export const popupFeatures = (
   width: number,
   height: number,
@@ -32,7 +42,27 @@ export const openPopup = (
 ) => {
   const popup = open('', 'loading', features)
   popup.document.write(html)
-  const stopCallback = waitForValue(() => popup.closed, true, onClose)
+  const stopCallback = waitForValue(
+    () => {
+      try {
+        return popup.closed
+      } catch (error) {
+        // we might encounter a cross origin error if the popup has navigated to a
+        // different domain, in which case we should ignore the error (so we don't pollute Sentry)
+        // and keep polling in case the context is reinstated
+        // Safari throws an error when accessing popup.closed if the popup has lost context
+        // (navigated to different domain)
+        // see https://gr4vy-tq.sentry.io/issues/7486375527/?alert_rule_id=15723108&alert_type=issue&notification_uuid=d68908db-b3c0-434c-b0db-640ed0ec7cdb&project=4508636754477056&referrer=slack
+        // this logic is here so we don't pollute Sentry, we might as well just ignore the error
+        // in the Sentry config, but then we would miss that SecurityError instances in legitimate cases
+        if (!isCrossOriginError(error)) {
+          throw error
+        }
+      }
+    },
+    true,
+    onClose
+  )
 
   if (timeout) {
     setTimeout(() => popup.close(), timeout)
